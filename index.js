e37fbdeb9986a1e23bf0c4b71b4d0b080ea4818c
Samaya Mughumova <81756158+Samaya20@users.noreply.github.com>
2024-07-15 16:51:29 +0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const studentsPath = path.join(__dirname, "db.json");
 
 app.use(cors({
-  origin: 'https://nodex-task-react.netlify.app', // Düzeltilmiş CORS ayarları
+  origin: 'https://nodex-task-react.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
